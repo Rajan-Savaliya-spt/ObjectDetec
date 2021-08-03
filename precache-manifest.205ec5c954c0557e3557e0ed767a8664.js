deacd7bb15894bd21321a8f0094f5bf2050c028a
Rajan-Savaliya-spt <rajan.savaliya@sneptech.com>
2021-08-03 14:40:36 +0530

    Updates

---
+++ precache-manifest.205ec5c954c0557e3557e0ed767a8664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc7c3e56b11bde5baa91b52349107ac",
+    "url": "/ObjectDetec/index.html"
+  },
+  {
+    "revision": "d10cf8c94ecdfa9a67be",
+    "url": "/ObjectDetec/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a91331ff8103ff08020",
+    "url": "/ObjectDetec/static/js/2.43189c52.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/ObjectDetec/static/js/2.43189c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10cf8c94ecdfa9a67be",
+    "url": "/ObjectDetec/static/js/main.2c7ff1a0.chunk.js"
+  },
+  {
+    "revision": "59e0016d07af3120b6fb",
+    "url": "/ObjectDetec/static/js/runtime-main.12ef2841.js"
+  }
+]);
